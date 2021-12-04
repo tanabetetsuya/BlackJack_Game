730db00dc12eacf278c79849c3a4d7b6194e8dc9
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-04 20:18:46 +0900

    [Add] buildの定義

--- deck.rb
+++ deck.rb
@@ -6,6 +6,16 @@ class Deck
     build
   end
 
+  #山札作成
+  def build
+    for suit in ["♡", "♤", "♢", "♧"] do
+      for number in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] do
+        card = Card.new(suit, number)
+        @cards << card
+      end
+    end
+  end
+
   # 山札の上から一枚引く
   def draw_card
     # @cardsの配列から一番最後の要素を削除する。削除された要素はpopメソッドの戻り値として渡される。
